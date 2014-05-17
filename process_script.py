6c377eb9523db6b075e461b7174e755daafca78d
Ilia Kurenkov <ilia.kurenkov@gmail.com>
2014-05-16 20:58:35 -0400

    minor fixes and docstrings in process_script.py

--- process_script.py
+++ process_script.py
@@ -4,29 +4,45 @@ from util import *
 
 
 def check_cond_item(entry, cond_range, item_range):
+	'''Checks if the condition and item members of the "entry" tuple are within
+	their respective permissible ranges.
+	'''
 	cond, item = entry[:2]
 	return int(cond) in cond_range and int(item) in item_range
 
 
-def tuples_to_str(seq):
-	return ('\t'.join(member) + '\n' for member in seq)
+def tuples_to_str(seq, sep='\t'):
+	'''Turns every member tuple of seq into a string where subparts of the tuple
+	are separated from each other by "sep" and each row ends in a newline.
+	'''
+	return (sep.join(member) + '\n' for member in seq)
 
 
 def write_out(exp_name, input_type, item_nums, cond_nums, data):
-	with open('all' + input_type + '.txt', 'w') as f:
+	'''Writes all the data collected to two files:
+	One with all the data and one with only a subset thereof that fits in the 
+	condition and item ranges specified by the user.
+	'''
+	all_file = '_'.join(['all', input_type, '.txt'])
+	with open(all_file, 'w') as f:
 		f.writelines(tuples_to_str(data))
 	filtered_data = (d for d in data if check_cond_item(d, cond_nums, item_nums))
-	with open(exp_name + input_type + '.txt', 'w') as f:
+	exp_file = '_'.join([exp_name, input_type, '.txt'])
+	with open(exp_file, 'w') as f:
 		f.writelines(tuples_to_str(filtered_data))
 
 
-def trigger_to_code(item):
+def trigger_to_code(question_item):
+	'''Given a question item, looks up the the number code for the left and right
+	triggers.
+	Then tacks on this code to the question item and returns it.
+	'''
 	codes = {
 	'leftTrigger' : '6',
 	'rightTrigger' : '7',
 	}
-	trigger = item[2].strip()
-	return item + (codes[trigger],)
+	trigger = question_item[2].strip()
+	return question_item + (codes[trigger],)
 
 
 def read_script_file(file_name):
